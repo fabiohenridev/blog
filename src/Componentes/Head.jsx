2ca70a7ec4863e99e9842c4e3f0dda41eb6fe0bb
fabio henrique <henri8274@gmail.com>
2025-02-14 16:16:24 -0300

    update

--- src/Componentes/Head.jsx
+++ src/Componentes/Head.jsx
@@ -1,14 +1,43 @@
+import { useState } from "react";
 import "./Head.css";
+import { Link } from "react-router-dom";
+
 
 export default function Head(){
+
+const [menu, setMenu] = useState(false);
+
+const OpenMenu = () =>{
+ setMenu(true);
+}
+
+const CloseMenu = () =>{
+    setMenu(false)
+}
+
     return(
         <div>
          <header className="header">
             <div className="title">
               <h1>O CURIOSO</h1>
             </div>
+            {menu && (
+
+           <div className="Categorias">
+            <div className="buttonClose">
+            <button onClick={CloseMenu}>x</button> 
+            </div>
+          <div className="Links">
+          <Link className="Link" to="/Paginainicial">Curiosidades</Link>
+          <Link className="Link" to="/Paginainicial">Curiosidades</Link>
+          <Link className="Link" to="/Paginainicial">Curiosidades</Link>
+          <Link className="Link" to="/Paginainicial">Curiosidades</Link>
+          </div>
+           </div>
+
+            )}
             <div className="buttonHead">
-                <button><i class="bi bi-list"></i></button>
+                <button onClick={OpenMenu}><i class="bi bi-list"></i></button>
             </div>
          </header>
         </div>
